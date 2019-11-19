bd242660f39c13a0e9d93959084ff5c64136593e
victorN <victornguyen082@gmail.com>
2019-11-19 16:04:08 -0500

    most of pages styling and functionality completed,
    Co-authored-by: Yusuf <yusufnafey@yahoo.com>

--- team-z-fe/src/components/Unauthorized.js
+++ team-z-fe/src/components/Unauthorized.js
@@ -13,8 +13,13 @@ export default class Unauthorized extends React.Component {
     render() {
         return (
             <div className="unauthorized-container">
-                <h1 className="unauthorized-title">You are unauthorized to view this page! Please login first to continue.</h1>
-                <h3 className="unauthorized-subtitle"><Link className='landing-link' to='/login'>Return to Login</Link></h3>
+                <h1 className="unauthorized-title">Error 403</h1>
+                <h3 className="unauthorized-subtitle">
+                    Unfortunately, you don't have the permissions required to access this page.<br /><br />Please
+                    <Link className="landing-link" to="/login"> login </Link>
+                    first or return to
+                    <Link className='landing-link' to='/'> home</Link>.
+                 </h3>
             </div>
         )
     }
