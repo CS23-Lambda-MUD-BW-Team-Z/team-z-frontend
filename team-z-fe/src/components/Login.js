d6ea39d280353c21ea39799fa9687802fead8af1
victorN <victornguyen082@gmail.com>
2019-11-21 21:05:22 -0500

    styling map

--- team-z-fe/src/components/Login.js
+++ team-z-fe/src/components/Login.js
@@ -1,22 +1,18 @@
 import React from "react";
 import axios from "axios";
 import { Link } from 'react-router-dom';
-
 export default class Login extends React.Component {
     state = {
         username: "",
         email: "",
         password: "",
     }
-
     handleChange = event => {
         this.setState({ [event.target.name]: event.target.value })
     };
-
     handleSubmit = event => {
         event.preventDefault();
         // this.props.login(this.state)
-        
         const endpoint = "https://lambda-university.herokuapp.com/api/login/"
         axios
             .post(endpoint, this.state)
@@ -30,32 +26,34 @@ export default class Login extends React.Component {
                 console.log("error", error);
             });
     }
-
     render() {
         return (
             <div className="login-container">
                 <div className="login-page">
-                    Please Log In
-                    <div className="login-form">
-                        <form onSubmit={(event) => this.handleSubmit(event)}>
-                            <input
-                                type="text"
-                                placeholder="username"
-                                onChange={this.handleChange}
-                                value={this.state.username}
-                                name="username"
-                            />
-                            <input
-                                type="password"
-                                placeholder="password"
-                                onChange={this.handleChange}
-                                value={this.state.password}
-                                name="password"
-                            />
-                            <button type="submit">Login</button>
-                            <button type="submit" onClick={()=>{this.props.history.push('/')}}>Go Back</button>
-                        </form>
-                    </div>
+                    <h1 className="title">Log In</h1>
+                    <p className="subtitle">Your journey awaits you.</p>
+                    <form onSubmit={(event) => this.handleSubmit(event)}>
+                        <input
+                            type="text"
+                            placeholder="username"
+                            onChange={this.handleChange}
+                            value={this.state.username}
+                            name="username"
+                        />
+                        <input
+                            type="password"
+                            placeholder="password"
+                            onChange={this.handleChange}
+                            value={this.state.password}
+                            name="password"
+                        />
+                        <button type="submit">Login</button>
+                        <button type="submit" onClick={() => { this.props.history.push('/') }}>Go Back</button>
+                    </form>
+                    <p className="login-register-link">
+                        Don't have an account yet?
+                        <Link className="login-redirect" to="/register"> Register here!</Link>
+                    </p>
                 </div>
             </div>
         )
--- team-z-fe/src/components/game/Game.js
+++ team-z-fe/src/components/game/Game.js
@@ -1,57 +1,57 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { props } from 'bluebird'
 import Movements from './Movements'
 import Map from './Map'
-function Game( {setUser, user, history,loginKey} ) {
-    const [rooms,setRooms] = useState([])
+function Game({ setUser, user, history, loginKey }) {
+    const [rooms, setRooms] = useState([])
     const [currentroom, setCurrentRoom] = useState()
 
-    useEffect(()=>{
-        if (localStorage.getItem('token')!=null) {
+    useEffect(() => {
+        if (localStorage.getItem('token') != null) {
             loginKey(localStorage.getItem('token'))
-        //   setUser(user)
-        console.log('true')
-          
+            //   setUser(user)
+            console.log('true')
+
         }
-      },[])
+    }, [])
 
-   
-    useEffect(()=> {
-        
+
+    useEffect(() => {
+
         axios.get('https://lambda-university.herokuapp.com/api/adv/init')
-        .then( res => {
-            console.log(res)
-            setUser({...res.data})
-        })
-        .catch( res => {
-            history.push('/')
-        })
+            .then(res => {
+                console.log(res)
+                setUser({ ...res.data })
+            })
+            .catch(res => {
+                history.push('/')
+            })
 
-    }, [axios,setUser])
+    }, [axios, setUser])
 
-    useEffect(()=>{
-        let grabber = async ()=>{
+    useEffect(() => {
+        let grabber = async () => {
             try {
                 let req = await axios.get('https://lambda-university.herokuapp.com/api/adv/rooms/')
                 let data = await req.data
                 // data[78].title = "Career Building Back Hallway"
                 // data[78].description = "Leads to the back of the Career Building"
-                
+
                 // for (let i=0; i< data.length; i++) {
                 //     console.log(data[i].id + `: ${i}`)
                 //     data[i].id = i
                 // }
                 setRooms(data)
-                
+
             } catch (error) {
-                console.error('couldn\'t fetch rooms',{...error});
+                console.error('couldn\'t fetch rooms', { ...error });
             }
         }
-        if(rooms.length==0){
+        if (rooms.length == 0) {
             grabber()
         }
-    },[rooms])
+    }, [rooms])
 
 
     const logout = () => {
@@ -60,18 +60,18 @@ function Game( {setUser, user, history,loginKey} ) {
         history.push('/')
     }
 
-    
+
     console.log(rooms)
     let current = rooms.filter(room => room.title === user.title && room.description === user.description)
-    
-    
+
+
     current = current[0]
-    
+
     current && localStorage.setItem('id', current.id)
 
-    
+
     //current && setCurrentRoom(current)
-    
+
     // if (current) {
     //     const currentroom = document.querySelector('.room-134')
     //     console.log(currentroom)
@@ -81,47 +81,48 @@ function Game( {setUser, user, history,loginKey} ) {
     // currentroom.classList.toggle('on')
 
     if (current && current.n_to === 0) { //if current room's 'n_to' points to 0, it is unavailable
-           // using local storage as using a state cause an infinite loop
-            localStorage.setItem('north', 'false') // set storage of north to false
-            
+        // using local storage as using a state cause an infinite loop
+        localStorage.setItem('north', 'false') // set storage of north to false
+
         /// repeat pattern below
-        } else {
-          
-            localStorage.setItem('north', 'true')
-        }
-    
-        if (current && current.s_to === 0) {
-            localStorage.setItem('south', 'false')
-        } else {
-            localStorage.setItem('south', 'true')
-        }
-    
-        if (current && current.e_to === 0) {
-            localStorage.setItem('east', 'false')
-        } else {
-            localStorage.setItem('east', 'true')
-        }
-    
-        if (current && current.w_to === 0) {
-            localStorage.setItem('west', 'false')
-        } else {
-            localStorage.setItem('west', 'true')
-        }
+    } else {
+
+        localStorage.setItem('north', 'true')
+    }
+
+    if (current && current.s_to === 0) {
+        localStorage.setItem('south', 'false')
+    } else {
+        localStorage.setItem('south', 'true')
+    }
+
+    if (current && current.e_to === 0) {
+        localStorage.setItem('east', 'false')
+    } else {
+        localStorage.setItem('east', 'true')
+    }
+
+    if (current && current.w_to === 0) {
+        localStorage.setItem('west', 'false')
+    } else {
+        localStorage.setItem('west', 'true')
+    }
 
     return (
         <div className="game-container">
-            
-            <Map rooms={rooms} current={current} />
+            <div className="outer-map-container">
+                <Map rooms={rooms} current={current} />
+            </div>
             <div className="bottom-section">
                 <div className="top-section">
-                <button onClick={logout} className="logout">Log Out</button>
+                    <button onClick={logout} className="logout">Log Out</button>
                     <h2>{user.name}</h2>
                     <h3>{user.title}</h3>
                     <p>{user.description}</p>
                 </div>
-                <Movements setUser={setUser}  />
+                <Movements setUser={setUser} />
             </div>
-            
+
         </div>
     )
 }
